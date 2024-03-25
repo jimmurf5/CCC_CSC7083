b5e2a234461e93cbd0df0d2782cb0c64d19e7cc7
12555053 <ccanning08@qub.ac.uk>
2024-03-25 15:32:30 +0000

    adjust field class and add further validation checks

--- CSC7083_SE_Assignment/src/climateChangeCrisis/Field.java
+++ CSC7083_SE_Assignment/src/climateChangeCrisis/Field.java
@@ -3,103 +3,140 @@ package climateChangeCrisis;
 import java.util.ArrayList;
 
 public class Field {
-	
-	//Instance vars
-	
+
+	// Constants for business rules
+
+	private static final int MIN_FIELDLENGTH = 10;
+	private static final int MAX_FIELDLENGTH = 30;
+
+	// Instance vars
+
 	private String fieldName;
 	private ArrayList<Area> areas;
-	private int areaCost;
+	private int areaBuyCost;
+	private int areaDonationCost;
 	private Player ownedBy;
-	private boolean isOwnedByPlayer;
-	
-	//Constructor 
-	
+
+	// Constructor
+
 	/**
-	 * Constructor for Field- sets ownedBy and isOwnedByPlayer to null and false respectively
+	 * Constructor for Field- sets ownedBy and isOwnedByPlayer to null and false
+	 * respectively
+	 * 
 	 * @param fieldName
 	 * @param areas
 	 * @param areaCost
 	 */
-	public Field(String fieldName, ArrayList<Area> areas, int areaCost) {
+	public Field(String fieldName, ArrayList<Area> areas, int areaBuyCost, int areaDonationCost) {
 		this.setFieldName(fieldName);
 		this.setAreas(areas);
-		this.setAreaCost(areaCost);
+		this.setareaBuyCost(areaBuyCost);
+		this.setareaDonationCost(areaDonationCost);
 		this.setownedBy(null);
-		this.setisOwnedByPlayer(false);
 	}
-	
-	//Getters & Setters
 
+	// Getters & Setters
 
 	public String getFieldName() {
 		return fieldName;
 	}
 
-	
-	public void setFieldName(String fieldName) {
+	/**
+	 * 
+	 * @param fieldName
+	 * @throws IllegalArgumentException if field name is outside business rules or
+	 *                                  trying to set null
+	 */
+	public void setFieldName(String fieldName) throws IllegalArgumentException {
+
+		if (fieldName == null) {
+			throw new IllegalArgumentException("Field name cannot be null");
+		}
+
+		if ((fieldName.length() < MIN_FIELDLENGTH) || (fieldName.length() > MAX_FIELDLENGTH)) {
+			throw new IllegalArgumentException("Field name is too long or too short");
+		}
+
 		this.fieldName = fieldName;
 	}
 
-	
 	public ArrayList<Area> getAreas() {
 		return areas;
 	}
 
-	
-	public void setAreas(ArrayList<Area> areas) {
+	/**
+	 * 
+	 * @param areas
+	 * @throws IllegalArgumentException if ArrayList passed in is null or empty
+	 */
+	public void setAreas(ArrayList<Area> areas) throws IllegalArgumentException {
+
+		if (areas == null) {
+			throw new IllegalArgumentException("Areas ArrayList cannot be null");
+		}
+		if (areas.size() < 1) {
+			throw new IllegalArgumentException("Areas ArrayList cannot be empty");
+		}
+
 		this.areas = areas;
 	}
 
-	
-	public int getAreaCost() {
-		return areaCost;
+	public int getareaBuyCost() {
+		return areaBuyCost;
 	}
 
-	
-	public void setAreaCost(int areaCost) {
-		this.areaCost = areaCost;
+	/**
+	 * 
+	 * @param buyAreaCost
+	 * @throws IllegalArgumentException if cost passed in is less than zero
+	 */
+	public void setareaBuyCost(int areaBuyCost) throws IllegalArgumentException {
+
+		if (areaBuyCost < 0) {
+			throw new IllegalArgumentException("Area buy cost cannot be less than zero");
+		}
+
+		this.areaBuyCost = areaBuyCost;
 	}
 
-	
-	public Player getOwnership() {
+	public int getareaDonationCost() {
+		return areaDonationCost;
+	}
+
+	/**
+	 * 
+	 * @param areaDonationCost
+	 * @throws IllegalArgumentException if cost passed in is less than zero
+	 */
+	public void setareaDonationCost(int areaDonationCost) throws IllegalArgumentException {
+
+		if (areaDonationCost < 0) {
+			throw new IllegalArgumentException("Area donation cost cannot be less than zero");
+		}
+
+		this.areaDonationCost = areaDonationCost;
+	}
+
+	public Player getownedBy() {
 		return ownedBy;
 	}
 
 	/**
 	 * 
 	 * @param ownedBy
-	 * @throws IllegalArgumentException if a Player already owns the Field
+	 * @throws IllegalArgumentException if a Player already owns the Field or the
+	 *                                  argument passed in is null
 	 */
 	public void setownedBy(Player ownedBy) throws IllegalArgumentException {
 		if (this.ownedBy != null) {
 			throw new IllegalArgumentException("Field is already owned by another Player");
 		}
-		
-		this.ownedBy = ownedBy;
-		this.setisOwnedByPlayer(true);
-	}
 
-	
-	public boolean isOwnedByPlayer() {
-		return isOwnedByPlayer;
-	}
-
-	/**
-	 * 
-	 * @param isOwnedByPlayer
-	 * @throws IllegalArgumentException if a Player already owns the Field
-	 */
-	public void setisOwnedByPlayer(boolean isOwnedByPlayer) throws IllegalArgumentException {
-		
-		if (this.isOwnedByPlayer) {
-			throw new IllegalArgumentException("Field is already owned by another Player");
+		if (ownedBy == null) {
+			throw new IllegalArgumentException("Cannot change Field ownership to null");
 		}
-		
-		this.isOwnedByPlayer = isOwnedByPlayer;
-	}
-	
-	
-	
 
-	
+		this.ownedBy = ownedBy;
+	}
+
 }
